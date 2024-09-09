a894b105db205df1c323944aaecd36d2edee1671
susanto_m <222011324@stis.ac.id>
2024-09-09 16:41:37 +0700

    add dynamic construct sql query

--- transform1.py
+++ transform1.py
@@ -4,7 +4,7 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.postgres_operator import PostgresOperator
 from airflow.providers.microsoft.mssql.hooks.mssql import MsSqlHook
-
+from psycopg2 import sql
 import pandas as pd
 
 #DEFINE THE DAG
@@ -75,14 +75,22 @@ def etl_data ():
     df['TotalProductCost'] = df['TotalProductCost'].astype(float)
     df['SalesAmount'] = df['SalesAmount'].astype(float)
     df['TaxAmt'] = df['TaxAmt'].astype(float)
-    
+
     #Write transformed data to destination PostgreSQL table
     # df.to_csv('/opt/bitnami/airflow/dags/transform1.csv',index=False)
+    # Convert DataFrame to tuples
     tuples = [tuple(x) for x in df.to_numpy()]
-    cols = ','.join(list(df.columns))
-    query = "INSERT INTO AggregationTransformation (%s) VALUES %%s" % (cols)
+    cols = df.columns.tolist()
+    
+    # Dynamically construct SQL query using psycopg2's sql module
+    query = sql.SQL("INSERT INTO AggregationTransformation ({}) VALUES ({})").format(
+        sql.SQL(', ').join(map(sql.Identifier, cols)),
+        sql.SQL(', ').join(sql.Placeholder() * len(cols))
+    )
+    
+    # Execute the query
     cursor = vercel_conn.cursor()
-    cursor.executemany(query, tuples)
+    cursor.executemany(query.as_string(vercel_conn), tuples)
     vercel_conn.commit()
     cursor.close()
     # df.to_sql('AggregationTransformation', con=vercel_conn, index=False, if_exists='append')
