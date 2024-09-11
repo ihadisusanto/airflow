ba0a101f20fed2667fd4c27a1103eefe00718cfd
susanto_m <222011324@stis.ac.id>
2024-09-11 16:27:30 +0700

    local-postgres

--- transform5.py
+++ transform5.py
@@ -68,7 +68,7 @@ ORDER BY pa.ipk DESC
 """
 
 def extract_data(**kwargs):
-    postgres_hook = PostgresHook(postgres_conn_id='local_postgres')
+    postgres_hook = PostgresHook(postgres_conn_id='local-postgres')
     conn = postgres_hook.get_conn()
     df = pd.read_sql(extract_query, conn=conn)
     kwargs['ti'].xcom_push(key='data', value=df)
